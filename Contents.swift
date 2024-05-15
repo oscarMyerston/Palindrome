9ac22f88f291b44fd1f836bdaaee64c059643573
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-05-14 21:55:22 -0500

    First commit

---
+++ Contents.swift
@@ -0,0 +1,36 @@
+/*
+ ___ _      _ _         _
+| _ \ |__ _| (_)_ _  __| |_ _ ___ _ __  ___ ___
+|  _/ / _` | | | ' \/ _` | '_/ _ \ '  \/ -_|_-<
+|_| |_\__,_|_|_|_||_\__,_|_| \___/_|_|_\___/__/
+
+ // Challenge: Detect if a given string is a palindrome.
+ // A palindrome is a word that can be spelt the same way forwards and backwards.
+
+ */
+
+func isPalindrome(_ text: String) -> Bool {
+    var textReversed: String = ""
+    textReversed = text
+    return text == String(textReversed.reversed())
+}
+
+isPalindrome("abba")    // true
+isPalindrome("mom")     // true
+isPalindrome("dad")     // true
+isPalindrome("radar")   // true
+isPalindrome("rush")    // false
+isPalindrome("yes")     // false
+
+
+func isPalindromee(_ text: String) -> Bool {
+    let chars = Array(text)
+    let length = chars.count
+
+    for i in 0..<length / 2 {
+        if chars[i] != chars[length - i - 1] {
+            return false
+        }
+    }
+    return true
+}
